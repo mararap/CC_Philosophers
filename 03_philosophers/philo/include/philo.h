b629338538387cc37a67e45fecb5a7234d66f31b
marapovi <marapovi@student.42vienna.com>
2025-11-25 20:19:24 +0100

    fo: FINISHED philo: folders and files created

---
+++ 03_philosophers/philo/include/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marapovi <marapovi@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/25 19:24:53 by marapovi          #+#    #+#             */
+/*   Updated: 2025/11/25 19:32:03 by marapovi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+#include <pthread.h>
+#include <sys/time.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#endif //!PHILO_H
