84073d1abf8988cd8ced8dce7d3e37eb7704206c
marapovi <marapovi@student.42vienna.com>
2026-04-08 14:04:23 +0200

    dominic mentoring

--- 03_philosophers/include/philo.h
+++ 03_philosophers/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: marapovi <marapovi@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 14:14:34 by marapovi          #+#    #+#             */
-/*   Updated: 2026/04/07 15:05:39 by marapovi         ###   ########.fr       */
+/*   Updated: 2026/04/08 13:14:23 by marapovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,42 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-typedef struct s_philo
+// typedef pthread_mutex_t t_fork;	// dominic tipp
+
+typedef struct s_fork t_fork;
+typedef struct s_philo t_philo;
+typedef struct s_dinner t_dinner;
+
+struct s_fork
+{
+	pthread_mutex_t	lock;
+};
+
+struct s_philo
 {
 	size_t		id;
-	pthread_t	tid;
-	// thread-handle
+	pthread_t	tid; // thread-handle
 	long long	last_meal_time;
 	size_t		meal_count;
-}			t_philo;
+	t_fork		*left;
+	t_fork		*right;
+	t_dinner	*dinner;
+};
 
-
+struct s_dinner
+{
+	size_t			number_of_philosophers;
+	long long		time_to_die;
+	long long		time_to_eat;
+	long long		time_to_sleep;
+	long long		start_time;
+	size_t			number_of_times_each_philosopher_must_eat;
+	size_t			meals_required;
+	t_philo			*philo_arr;
+	t_fork			*forks_arr;
+	int				dead;
+	pthread_mutex_t	print;
+};
 
 
 // Add your structs, function prototypes, and macros here
