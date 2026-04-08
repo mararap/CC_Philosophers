a32d06d714df63fef07f75fd503517a768117807
marapovi <marapovi@student.42vienna.com>
2026-04-08 21:56:01 +0200

    removed error.h and utils.h, wrote utils-functions

--- 03_philosophers/include/philo.h
+++ 03_philosophers/include/philo.h
@@ -6,54 +6,49 @@
 /*   By: marapovi <marapovi@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 14:14:34 by marapovi          #+#    #+#             */
-/*   Updated: 2026/04/08 13:14:23 by marapovi         ###   ########.fr       */
+/*   Updated: 2026/04/08 21:08:53 by marapovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-#include <pthread.h>
-#include <sys/time.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
 
-// typedef pthread_mutex_t t_fork;	// dominic tipp
-
-typedef struct s_fork t_fork;
-typedef struct s_philo t_philo;
-typedef struct s_dinner t_dinner;
-
-struct s_fork
-{
-	pthread_mutex_t	lock;
-};
+typedef pthread_mutex_t	t_fork_lock;
+typedef struct s_philo	t_philo;
+typedef struct s_dinner	t_dinner;
 
 struct s_philo
 {
-	size_t		id;
-	pthread_t	tid; // thread-handle
-	long long	last_meal_time;
-	size_t		meal_count;
-	t_fork		*left;
-	t_fork		*right;
-	t_dinner	*dinner;
+	int					id;
+	pthread_t			tid; // thread-handle
+	long long			last_meal_time;
+	int					meal_count;
+	t_fork_lock			*left;
+	t_fork_lock			*right;
+	t_dinner			*dinner;
 };
 
 struct s_dinner
 {
-	size_t			number_of_philosophers;
-	long long		time_to_die;
-	long long		time_to_eat;
-	long long		time_to_sleep;
-	long long		start_time;
-	size_t			number_of_times_each_philosopher_must_eat;
-	size_t			meals_required;
-	t_philo			*philo_arr;
-	t_fork			*forks_arr;
-	int				dead;
-	pthread_mutex_t	print;
+	int					philo_count;
+	long long			time_to_die;
+	long long			time_to_eat;
+	long long			time_to_sleep;
+	long long			start_time;
+	int					meals_required;
+	t_philo				*philo_arr;
+	t_fork_lock			*forks_arr;
+	int					is_dead;
+	pthread_mutex_t		print_lock;
+	pthread_mutex_t		dead_lock;
+	pthread_mutex_t		meal_lock;
 };
 
 
