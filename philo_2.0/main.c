19dd7c89bfe60229bd5b14ee742c23a19fd10b2a
marapovi <marapovi@student.42vienna.com>
2026-05-04 13:59:09 +0200

    made a fresh start in philo_2.0. rewrote philo.h and main.c

---
+++ philo_2.0/main.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marapovi <marapovi@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/04 13:41:38 by marapovi          #+#    #+#             */
+/*   Updated: 2026/05/04 13:58:03 by marapovi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static int	ph_parse_args(int ac, char **av, t_dinner *d)
+{
+	int	i;
+
+	if (ac < 5 || ac > 6)
+	{
+		write(2, "invalid input\nusage: ./philo N ttd tte tts [meals]\n", 52);
+		return (1);
+	}
+	i = 1;
+	while (i < ac)
+	{
+		if (ph_atoull(av[i]) == -1)
+			return (write(2, "invalid argument(s)\n", 21), 1);
+		i++;
+	}
+	d->philo_count = (int)ph_atoull(av[1]);
+	if (d->philo_count <= 0)
+		return (write(2, "invalid input\n", 15), 1);
+	d->ttd = ph_atoull(av[2]) * 1000;
+	d->tte = ph_atoull(av[3]) * 1000;
+	d->tts = ph_atoull(av[4]) * 1000;
+	if (av[5])
+		d->meals_required = (int)ph_atoull(av[5]);
+	else
+		d->meals_required = -1;
+	return (0);
+}
+
+int	main(int ac, char **av)
+{
+	t_dinner	dinner;
+	pthread_t	monitor_tid;
+	
+	if(ph_parse_args(ac, av, &dinner) != 0)
+		return (1);
+	if (ph_init_dinner(&dinner) != 0)
+		return (1);
+	if (ph_init_forks(&dinner) != 0)
+		return(ph_cleanup(&dinner, 3), 1);
+	dinner.start_time = ph_get_time_us();
+	ph_init_philos(&dinner);
+	if (ptread_create(&monitor_tid, NULL, ph_monitor, &dinner) != 0)
+		return (ph_cleanup(&dinner, 3), 1);
+	ph_start_threads(&dinner);
+	pthread_join(monitor_tid, NULL);
+	ph_cleanup(&dinner, 3);
+	return (0);
+}
---
+++ philo_2.0/philo.h
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marapovi <marapovi@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/04 13:29:44 by marapovi          #+#    #+#             */
+/*   Updated: 2026/05/04 13:51:23 by marapovi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <limits.h>
+# include <pthread.h>
+# include <stdio.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+typedef pthread_mutex_t	t_fork;
+typedef struct s_philo	t_philo;
+typedef struct s_dinner	t_dinner;
+
+struct s_philo
+{
+	int				id;
+	pthread_t		tid;
+	long long		last_meal;
+	long long		meal_count;
+	t_fork			*left;
+	t_fork			*right;
+	t_dinner		*dinner;
+};
+
+struct s_dinner
+{
+	int				philo_count;
+	long long		ttd;
+	long long		tte;
+	long long		tts;
+	long long		start_time;
+	int				meals_required;
+	t_philo			*philos;
+	t_fork			*forks;
+	pthread_mutex_t	print_lock;
+	pthread_mutex_t	done_lock;
+	pthread_mutex_t	meal_lock;
+};
+
+/* utils.c */
+long long			ph_atoull(const char *str);
+long long			ph_get_time_us(void);
+void				ph_print(t_philo *p, const char *msg);
+
+/* init.c */
+int					ph_init_dinner(t_dinner *d);
+int					ph_init_forks(t_dinner *d);
+void				ph_init_philos(t_dinner *d);
+
+/* threads.c */
+int					ph_start_treads(t_dinner *d);
+int					ph_is_done(t_dinner *d);
+
+/* actions.c */
+void				ph_take_forks(t_philo *p);
+void				ph_eat(t_philo *p);
+void				ph_drop_forks(t_philo *p);
+void				ph_sleep(t_philo *p);
+void				ph_think(t_philo *p);
+
+/* monitor.c */
+void				ph_monitor(void *arg);
+
+/* cleanup.c */
+void				ph_cleanup(t_dinner *d, int stage);
+
+#endif /* !PHILO_H */
